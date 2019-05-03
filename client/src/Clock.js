e4d0d0f934fe24249423af06c69ba15738e26ebe
Logan Jewett <logan.e.jewett@gmail.com>
2019-05-03 00:26:38 -0500

    started work on client adding a clock component

---
+++ client/src/Clock.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Clock extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>I'm a Clock</p>
+            </div>
+        )
+    }
+}
+
+export default Clock;
