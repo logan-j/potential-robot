209beeecd65d0949c9924ee96703a124a2d23ca7
Logan Jewett <logan.e.jewett@gmail.com>
2019-05-02 23:22:09 -0500

    first test against ping

--- server/server/api/tests.py
+++ server/server/api/tests.py
@@ -1,1 +1,7 @@
 from django.test import TestCase
+
+class ApiTestClass(TestCase):
+    def test_ping(self):
+        response = self.client.get('/api/ping')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b'pong')
